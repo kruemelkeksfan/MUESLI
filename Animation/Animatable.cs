e03f4365845950e9f2424070f5c370b0a39d7d4c
kruemelkeksfan <37933687+kruemelkeksfan@users.noreply.github.com>
2020-07-05 20:37:00 +0200

    Minor Changes to Animation Framework.

--- Animation/Animatable.cs
+++ Animation/Animatable.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Animatable : MonoBehaviour
-	{
+{
 	[SerializeField] private Vector3[] positions = null;
 	[SerializeField] private Vector3[] rotations = null;
 	private Vector3 startposition = Vector3.zero;
@@ -14,21 +14,21 @@ public class Animatable : MonoBehaviour
 	private float time = 0.0f;
 
 	private void Update()
-		{
+	{
 		if(passedtime < time)
-			{
+		{
 			passedtime += Time.deltaTime;
 
 			float progress = passedtime / time;
 			transform.position = Vector3.Lerp(startposition, targetposition, progress);
 			transform.rotation = Quaternion.Slerp(startrotation, targetrotation, progress);
-			}
 		}
+	}
 
 	public void Move(int target, float time)
-		{
+	{
 		if(target >= 0 && target < positions.Length && target < rotations.Length)
-			{
+		{
 			startposition = transform.position;
 			startrotation = transform.rotation;
 			targetposition = positions[target];
@@ -36,10 +36,10 @@ public class Animatable : MonoBehaviour
 
 			passedtime = 0.0f;
 			this.time = time;
-			}
+		}
 		else
-			{
+		{
 			Debug.LogError("Invalid Target Position " + target + " for " + name + "!");
-			}
 		}
 	}
+}
